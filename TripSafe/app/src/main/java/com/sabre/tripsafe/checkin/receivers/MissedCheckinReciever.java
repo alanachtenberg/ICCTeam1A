304b65fbb7480626d7e9100384640fa978b9eb09
alanachtenberg <alanachtenberg@tamu.edu>
2015-07-30 14:43:32 -0500

    refactored CheckInPreferences to allow multiple emails and phone numbers

--- TripSafe/app/src/main/java/com/sabre/tripsafe/checkin/receivers/MissedCheckinReciever.java
+++ TripSafe/app/src/main/java/com/sabre/tripsafe/checkin/receivers/MissedCheckinReciever.java
@@ -3,7 +3,10 @@ package com.sabre.tripsafe.checkin.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+
+import com.sabre.tripsafe.checkin.time.event.MissedEvent;
 
 import static com.sabre.tripsafe.MainActivity.TAG;
 
@@ -16,6 +19,9 @@ public class MissedCheckinReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "Missed CheckIn Alarm Broadcast recieved");
+        Bundle extras = intent.getExtras();
+        String[] emails = (String[])extras.get(MissedEvent.EMAIL_KEY);
+        String[] phones = (String[])extras.get(MissedEvent.PHONE_KEY);
         //TODO send notification via sms or mms
     }
 }
