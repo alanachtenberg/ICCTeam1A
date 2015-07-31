2ad7baf5b11c079d48e3811883fca86454fe45ec
rsitisr <tsirist@gmail.com>
2015-07-31 14:01:41 -0500

    email and text options implemented

--- TripSafe/app/src/main/java/com/sabre/tripsafe/alerts/BasicOption.java
+++ TripSafe/app/src/main/java/com/sabre/tripsafe/alerts/BasicOption.java
@@ -1,5 +1,15 @@
 package com.sabre.tripsafe.alerts;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.sabre.tripsafe.R;
+
 /**
  * Created by rsitisr on 2015-07-28.
  */
@@ -12,6 +22,30 @@ public class BasicOption implements Option {
     public BasicOption(boolean enabled, int threshold) {
         this.enabled = enabled;
         this.threshold = threshold;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent, Context mCtx) {
+        final int p = position;
+        LayoutInflater inflater = (LayoutInflater) mCtx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.view_basic_option, parent, false);
+        final CheckBox checkBox = (CheckBox) rowView.findViewById(R.id.checkBox);
+        final Button button = (Button) rowView.findViewById(R.id.button);
+        button.setText(Integer.toString(getThreshold()));
+        checkBox.setChecked(isEnabled());
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBox checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+                if (checkBox.isChecked())
+                    setEnabled(true);
+                else
+                    setEnabled(false);
+            }
+        });
+
+        return rowView;
     }
 
     @Override
